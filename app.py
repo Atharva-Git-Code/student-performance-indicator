f4ec6ecef29454be25d64efe7e06a87db50c5dab
Atharva-Git-Code <atharvafulmamdikar@gmail.com>
2023-04-22 17:20:27 +0530

    Commit

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ def predict_datapoint():
     else:
         data=CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
+            race_ethnicity=request.form.get('race/ethnicity'),
+            parental_level_of_education=request.form.get('parental level of education'),
             lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            test_preparation_course=request.form.get('test preparation course'),
+            reading_score=float(request.form.get('writing score')),
+            writing_score=float(request.form.get('reading score'))
 
         )
         pred_df=data.get_data_as_data_frame()
