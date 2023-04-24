39398306efa99c937130186d400984a957f0640e
Atharva-Git-Code <92671125+Atharva-Git-Code@users.noreply.github.com>
2023-04-24 14:28:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app=application
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('home.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
