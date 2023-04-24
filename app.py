c4ab9bb1658e2ed76084e4633388f63c14c18f2c
Atharva-Git-Code <92671125+Atharva-Git-Code@users.noreply.github.com>
2023-04-24 22:03:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ app=application
 
 @app.route('/')
 def index():
-    return render_template('home.html') 
+    return render_template('index.html') 
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('index.html')
+        return render_template('home.html')
     else:
         data=CustomData(
             gender=request.form.get('gender'),
